738cc4d8e84ad5fd46fb649dab4038dee10d90eb
Alexander Lichter <manniL@gmx.net>
2018-09-23 10:25:24 +0100

    Narrow down reproduction code

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,5 @@
 export default {
   build: {
-    extractCSS: true,
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          styles: {
-            name: 'styles',
-            test: /\.(css|vue)$/,
-            chunks: 'all',
-            enforce: true
-          }
-        }
-      }
-    }
+    extractCSS: true
   }
 }
--- pages/other.vue
+++ pages/other.vue
@@ -2,16 +2,6 @@
 <h1>No red color but green bg</h1>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
 <style>
 h1 {
  background-color: green
