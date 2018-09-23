732cbba19d2f36fce70c5a9df1c135bdc63b7a1b
Alexander Lichter <manniL@gmx.net>
2018-09-23 10:17:11 +0100

    Remove logo component

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,6 @@
 <h1>No green background but red color</h1>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
 <style>
 h1 {
  color: red
